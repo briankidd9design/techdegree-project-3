a77077f92a160eb9d466aa695cd328f238fef4e6
Brian Kidd <brian.kidd.one@gmail.com>
2018-11-20 09:38:48 -0800

    disable and enable working
    If there are conflicting days and time disable and enable are now working!

--- js/app.js
+++ js/app.js
@@ -3,361 +3,175 @@ Treehouse Techdegree:
 FSJS project 3 - Interactive Form
 ******************************************/
 //alert("Hello");
-$(document).ready(function (){
+$(document).ready(function() {
 	//focus on Name field
 	$("#name").focus();
-//Job Role Section
-//Hide otherTitle Id unless the user selects an other job title
+	//Job Role Section
+	//Hide otherTitle Id unless the user selects an other job title
 	$("#otherTitle").hide();
-
-	$("#title").change(function(){
-		if($("#title option:selected").val() === "other"){
+	$("#title").change(function() {
+		if ($("#title option:selected").val() === "other") {
 			$("#otherTitle").show();
-		}
-		else{
+		} else {
 			$("#otherTitle").hide();
 		}
 	});
-	
-	
-/*hide colors label and t-shirt select menu until t-shirt disign is chosen*/
-$("#colors-js-puns").hide();
-  $("#design").on("change", function(){
-	if ( $(this).val() === "js puns" || $(this).val() === "heart js" ) {
-		
-		$("#colors-js-puns").show();
-	}else{
-		$("#colors-js-puns").hide();
-	}
-	
-});
-
-//T-Shirt Info Section
+	/*hide colors label and t-shirt select menu until t-shirt disign is chosen*/
+	$("#colors-js-puns").hide();
+	$("#design").on("change", function() {
+		if ($(this).val() === "js puns" || $(this).val() === "heart js") {
+			$("#colors-js-puns").show();
+		} else {
+			$("#colors-js-puns").hide();
+		}
+	});
+	//T-Shirt Info Section
 	function hideOption(number) {
 		$('#colors-js-puns select option').eq(number).hide();
-		
 	}
 
 	function showOption(number) {
 		$('#colors-js-puns select option').eq(number).show();
-	
 	}
-		
-	$("#design").change(function(){
-
-		 if( $("#design option:selected").val() === "js puns"   ){ 
-			
+	$("#design").change(function() {
+		if ($("#design option:selected").val() === "js puns") {
 			$('#colors-js-puns select option')[0].selected = true;
-			
 			hideOption(3);
 			hideOption(4);
 			hideOption(5);
-		
 			showOption(0);
 			showOption(1);
 			showOption(2);
-		}
-		
-		 else if( $("#design option:selected").val() === "heart js"   ){
-			 
+		} else if ($("#design option:selected").val() === "heart js") {
 			$('#colors-js-puns select option')[3].selected = true;
-			
 			hideOption(0);
 			hideOption(1);
 			hideOption(2);
-		
 			showOption(3);
 			showOption(4);
 			showOption(5);
-			
-		} 
+		}
 	});
-	
 	//Register for Activities Section
-	//variable used to add up the cost of classes
-	
 	//My goals for the "Activities Section" is as follows
-	
 	//1.loop through fieldset inputs with .activities class
 	//2. be able to compare the day and times of each activity as opposed to just disabling certain checkboxes when a specific checkbox
 	//with conflicting time is checked. This way the code will work even if activities are added to the form. 
 	//var totalCost = 0;	
-	var totalCost = 0;//this is in the global scope. Is there a way to make it work in the function below?
-	 $(".activities input[type=checkbox]").on('change', function(e){ 
-				
-		
-				
-				
-			let inputLength = $('input[type="checkbox"]:checked').length;
-			console.log("input Length is");
-			console.log(inputLength);
-			
-			let checkboxText = ( $(e.target).parent().text() );
-			
-			console.log("This is the checkbox text index of the $");
-			/*Activity Day*/
-			/*This function takes in the whole description of the activity and finds the day of the week*/
-			let getActivityDay = function(checkboxText){
-				
-				let daysOfWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-				
-				for (let i = 0; i < daysOfWeek.length; i++){
-					
-					console.log (checkboxText);
-					
-				//	console.log(daysOfWeek[i]);
-					
-					if(checkboxText.includes(daysOfWeek[i])){
-						console.log("This is the Day of the week of the checked activity");
-						console.log(daysOfWeek[i]);
+	var totalCost = 0; //this is in the global scope. Is there a way to make it work in the function below?
+	$(".activities input[type=checkbox]").on('change', function(e) {
+		let inputLength = $('input[type="checkbox"]:checked').length;
+		let checkboxText = ($(e.target).parent().text());
+		/*Activity Day*/
+		/*This function takes in the whole description of the activity and finds the day of the week*/
+		let getActivityDay = function(checkboxText) {
+			let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+			for (let i = 0; i < daysOfWeek.length; i++) {
+				if (checkboxText.includes(daysOfWeek[i])) {
+					let dayOfWeekReturn = daysOfWeek[i];
+					return dayOfWeekReturn;
+				}
+			}
+		}
+		var activityDay = getActivityDay(checkboxText);
+		/*Activity Time*/
+		let timeSearch = /\b((?:1[0-2]|[1-9])[ap]m)-((?:1[0-2]|[1-9])[ap]m)/g;
+		let activityTime = checkboxText.match(timeSearch);
+		/*Activity Cost*/
+		let dollarSign = (checkboxText.indexOf("$"));
+		let activityCost = parseInt(checkboxText.slice((dollarSign + 1), (dollarSign + 4)));
+		$('.activities').append('<div id="activitiesTotal"></div>');
+		var updateCost = function(activityCost) {
+			totalCost += activityCost;
+			document.getElementById("activitiesTotal").innerHTML = "Total: $" + totalCost;
+		};
+		if ($(this).is(":checked")) {
+			updateCost(activityCost);
+		} else {
+			updateCost(-activityCost)
+		}
+		/*looping over the acitivites */
+		/*this section is used to count the number of checkboxes checked*/
+			let checkboxCheckedCount = 0;
+			let checkboxChecked = ($(this).is(":checked") );
+			let checkboxCheckedCounter = function (checkboxChecked){
+				if(checkboxChecked ){
+					checkboxCheckedCount++;
+				}
+			}
+			let countCheckedBoxes = checkboxCheckedCounter(checkboxChecked);
+			console.log("checkboxChecked count");
+			console.log(checkboxCheckedCount);
+		/*********************************************************************/
+		$(".activities input[type=checkbox]").each(function(index, value) {
+			let checkboxTextCheck = $(value).parent().text();
+			let specificEventTime = checkboxTextCheck.match(timeSearch)
+			let getActivityDays = function(checkboxTextCheck) {
+				let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+				for (let i = 0; i < daysOfWeek.length; i++) {
+					if (checkboxTextCheck.includes(daysOfWeek[i])) {
 						let dayOfWeekReturn = daysOfWeek[i];
-						return dayOfWeekReturn;	
+						return dayOfWeekReturn;
 					}
+				}
+			}
+			let activityDays = getActivityDays(checkboxTextCheck);
+		
+
+		if (checkboxCheckedCount === 1) {
+			if (specificEventTime != null && !(checkboxTextCheck === checkboxText)) {
+				if (activityDay === activityDays && JSON.stringify(activityTime) === JSON.stringify(specificEventTime)) {
+					$(value).attr("disabled", true);
+					$(value).parent().addClass("disabled");
 				
 				}
 			}
 			
-			var activityDay = getActivityDay(checkboxText);
-			console.log("This is the retun of the activity day");
-			console.log(activityDay);
+		} else {
 			
-			
-			/*Activity Time*/
-			let timeSearch = /\b((?:1[0-2]|[1-9])[ap]m)-((?:1[0-2]|[1-9])[ap]m)/g;
-			let activityTime = checkboxText.match(timeSearch);
-			
-			/*Activity Cost*/
-			let dollarSign = (checkboxText.indexOf("$") );
-			//console.log(dollarSign);
-			let activityCost = parseInt(checkboxText.slice( (dollarSign+1), (dollarSign+4) ) );
-			console.log("Cost of checked activity");
-			console.log(activityCost);
-			
-	$('.activities').append('<div id="activitiesTotal"></div>');
-			var updateCost = function (activityCost) {
-				totalCost += activityCost;
-				document.getElementById("activitiesTotal").innerHTML = "Total: $" + totalCost;
-			};  
+			if (activityDay === activityDays && JSON.stringify(activityTime) === JSON.stringify(specificEventTime)) {
+				console.log("activityDay and activityDays AND activityTime and sepcificEventTime");
+				console.log(activityDay + " " + activityDays + " " + JSON.stringify(activityTime) + " " + JSON.stringify(specificEventTime) );
+					$(value).attr("disabled", false);
+					$(value).parent().removeClass("disabled");
+			}
 		
-			if( $(this).is( ":checked") ){
-				updateCost(activityCost);
-					//console.log( $(e.target).parent().text() );
-					console.log("checkbox text inside the getCheckBoxText");
-					console.log(checkboxText);
-					console.log("$this is checked");
-					console.log($(this) + "is selected");
-					console.log("Activity time of the CHECKED activity is");
-					console.log(activityTime); 
-				}
-				else{
-					updateCost(-activityCost)
-				}
-		/*looping over the acitivites */
+		}
 		
-		$(".activities input[type=checkbox]").each(function(index, value) {
-				
+		
+		});
+	});
 	
-			let checkboxTextCheck = $(value).parent().text();
-					//	let specificEventDay = checkboxTextCheck.slice ( (emDash + 2), (emDash+6) );
-			let specificEventTime = checkboxTextCheck.match(timeSearch)
-					
-				console.log("checked event time is equal to");
-				console.log(activityTime);
-						
-			let getActivityDays = function(checkboxTextCheck){
-					
-				let daysOfWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-						
-				for (let i = 0; i < daysOfWeek.length; i++){
-							
-					console.log (checkboxTextCheck);
-							
-						//	console.log(daysOfWeek[i]);
-							
-					if(checkboxTextCheck.includes(daysOfWeek[i])){
-					console.log("The day of week for this activity is");
-					console.log(daysOfWeek[i]);
-					let dayOfWeekReturn = daysOfWeek[i];
-					return dayOfWeekReturn;	
-				}
-					
+	/*Payment Info section*/
+	/* this codes sets the default display to credit card*/
+	let defaultPaymentDisplay = function() {
+		if ($("#payment").val() === 'select_method') {
+			$("#payment").children().eq(0).hide();
+			$("#payment").children().eq(1).attr("selected", true);
+			$("#credit-card").show();
+			$("#credit-card").next().hide();
+			$("#credit-card").next().next().hide();
 		}
 	}
-					
-					let activityDays = getActivityDays(checkboxTextCheck);
-					console.log(activityDays);
-				
-					
-					
-					console.log ("checkboxTextCheck is");
-					console.log(checkboxTextCheck);
-					/* console.log ("Specific event day is equal to ");
-					console.log (specificEventDay); */
-					console.log("specific Event time is equal to");
-					console.log(specificEventTime);
-					
-					if ( $(this).is(":checked") && checkboxTextCheck === checkboxText){
-						console.log("checkboxText is equql to checkboxTextCheck")
-						
-						console.log(checkboxText + "is equal to " + checkboxTextCheck);
-					}
-					
-					
-					
-					
-				//	if($(this).is(":checked")){
-					if (  specificEventTime != null && !(checkboxTextCheck === checkboxText)){
-						
-						 if ( activityDay === activityDays &&  JSON.stringify(activityTime) === JSON.stringify(specificEventTime) ){
-							if ($(this).is(":checked")){
-					
-								$(value).attr("disabled", true);
-								
-							 } else{
-								$(value).attr("disabled", false);
-							} 
-						}
-					}
-				});
-	 });
-	
-	//let  totalCost = 0;
-	
-	var jsFrameworks = $("input[name='js-frameworks'");
-	var jsLibraries = $("input[name='js-libs']");
-	var express = $("input[name='express']");
-	var nodeJS = $("input[name='node']");
-	
-	
-
-	//var disabled = $('<style> .activities label{color: grey; text-decoration: line-through</style>'});
-	//let disabled = $('<style>.disabled{color:grey;}</style>');
-	//let disabled = $(this).css("color": "grey");
-
-	/* let disabled = $('<style> .disabled{color:grey;}</style>'); */
-	
-  	// Add total cost of activities
-
-/* 	$('.activities').append('<div id="activitiesTotal"></div>');
-
-	var updateCost = function (cost) {
-		totalCost += cost;
-		document.getElementById("activitiesTotal").innerHTML = "Total: $" + totalCost;
-	};  
-
-	$("input[name='all']").on('change', function () {
-		if ($(this).is(":checked")) {
-			updateCost(200);
-		} else {
-			updateCost(-200);
-		}
-	});
-
-	jsFrameworks.on('change', function () {
-		if ($(this).is(":checked")) {
-			express.prop("disabled", true);
-			express.parent().addClass("disabled");
-			updateCost(100);
-		} else {
-			express.prop("disabled", false);
-			express.parent().removeClass("disabled");
-			updateCost(-100);
-		}
-	});
-
-	jsLibraries.on ('change', function () {
-		if ($(this).is(":checked")) {
-			nodeJS.prop("disabled", true);
-			nodeJS.parent().addClass("disabled");
-			updateCost(100);
-		} else {
-			nodeJS.prop("disabled", false);
-			nodeJS.parent().removeClass("disabled");
-			updateCost(-100);
-		}
-	});
-
-	express.on('change',function () {
-		if ($(this).is(":checked")) {
-			jsFrameworks.prop("disabled", true);
-			jsFrameworks.parent().addClass("disabled");
-			updateCost(100);
-		} else {
-			jsFrameworks.prop("disabled", false);
-			jsFrameworks.parent().removeClass("disabled");
-			updateCost(-100);
-		}
-	});
-
-	nodeJS.on('change',function () {
-		if ($(this).is(":checked")) {
-			jsLibraries.prop("disabled", true);
-			jsLibraries.parent().addClass("disabled");
-			updateCost(100);
-		} else {
-			jsLibraries.prop("disabled", false);
-			jsLibraries.parent().removeClass("disabled");
-			updateCost(-100);
-		}
-	});
-
-	$("input[name='build-tools']").on('change', function () {
-		if ($(this).is(":checked")) {
-			updateCost(100);
-		} else {
-			updateCost(-100);
-		}
-	});
-
-	$("input[name='npm']").on('change', function () {
-		if ($(this).prop("checked")) {
-			updateCost(100);
-		} else {
-			updateCost(-100);
-		}
-	});
- */
-/*Payment Info section*/
-/* this codes sets the default display to credit card*/	
- let defaultPaymentDisplay = function () {
-	if($("#payment").val()==='select_method'){
-		$("#payment").children().eq(0).hide();
-		$("#payment").children().eq(1).attr("selected", true);
-		$("#credit-card").show();
-		$("#credit-card").next().hide();
-		$("#credit-card").next().next().hide();
-	}
-
-}
-/*invoking the desfualPayment Display method to show credit card as the default payment option */
-defaultPaymentDisplay(); 
-
-$("#payment").on("change", function(){
-	
-	if($(this).val()==='select_method'){
-		$("#credit-card").hide();
-		$("#credit-card").next().hide();
-		$("#credit-card").next().next().hide();
-	}
-	
-	else if($(this).val()==='credit card'){
-		$("#credit-card").show();
-		$("#credit-card").next().hide();
-		$("#credit-card").next().next().hide();
-	}
-	else if ($(this).val()==='paypal'){
+	/*invoking the desfualPayment Display method to show credit card as the default payment option */
+	defaultPaymentDisplay();
+	$("#payment").on("change", function() {
+		if ($(this).val() === 'select_method') {
 			$("#credit-card").hide();
-		$("#credit-card").next().show();
-		$("#credit-card").next().next().hide();
-	}
-	else if ($(this).val()==='bitcoin'){
-		$("#credit-card").hide();
-		$("#credit-card").next().hide();
-		$("#credit-card").next().next().show();
-	}
-	
-});
-
-	
-
+			$("#credit-card").next().hide();
+			$("#credit-card").next().next().hide();
+		} else if ($(this).val() === 'credit card') {
+			$("#credit-card").show();
+			$("#credit-card").next().hide();
+			$("#credit-card").next().next().hide();
+		} else if ($(this).val() === 'paypal') {
+			$("#credit-card").hide();
+			$("#credit-card").next().show();
+			$("#credit-card").next().next().hide();
+		} else if ($(this).val() === 'bitcoin') {
+			$("#credit-card").hide();
+			$("#credit-card").next().hide();
+			$("#credit-card").next().next().show();
+		}
+	});
 });
